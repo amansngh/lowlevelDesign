3ac036ef54af3d7fab2e73221d038a6d3a5930ba
Aman Singh <siaman@microsoft.com>
2024-04-21 17:20:14 +0530

    C# console app

---
+++ src/C#_ConsoleApp/C#_ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample");
+            Console.ReadKey();
+        }
+
+    }
+}
