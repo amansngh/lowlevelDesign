50fbd9a3a99510948192155acdf275f376d2948e
Aman Singh <siaman@microsoft.com>
2024-04-21 17:31:27 +0530

    Split and Map

--- src/C#_ConsoleApp/C#_ConsoleApp/Program.cs
+++ src/C#_ConsoleApp/C#_ConsoleApp/Program.cs
@@ -12,6 +12,22 @@ namespace C__ConsoleApp
         {
             Console.WriteLine("Sample");
             Console.ReadKey();
+            
+            //MAP
+            Dictionary<string, string> hashMap = new Dictionary<string, string>();
+            hashMap.Add("Key", "Value");
+            hashMap.Add("Gigi", "IDid");
+            //foreach (string key in hashMap.Keys) { Console.WriteLine(hashMap[key]); }
+            
+            //Split
+            String s = "Aman, Shyam, Ram, Lakhan";
+            String[] s2 = s.Split(',');
+            //foreach (string s3 in s2) { Console.WriteLine(s3); }
+
+            //substring
+            string subS = s.Substring(4, 10);
+            Console.WriteLine(subS);
+            Console.ReadKey();
         }
 
     }
